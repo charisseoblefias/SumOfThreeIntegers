475be5445bcebad2c8455d2348cbb9e3c8d9042a
char-kyeopta <charisse.oblefias01@gmail.com>
2022-08-09 12:31:07 +0800

    Add project files.

---
+++ SumOfThreeIntegers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class SumOfThreeIntegers
+{
+	static void Main(String [] args)
+	{
+		Console.Write ("Enter first integer: ");
+		int a = Int32.Parse (Console.ReadLine());
+		Console.Write ("Enter second integer: ");
+		int b = Int32.Parse (Console.ReadLine());
+		Console.Write ("Enter third integer: ");
+		int c = Int32.Parse (Console.ReadLine());
+		Console.WriteLine ("The sum is {0}", a + b + c);
+		Console.ReadKey();
+		
+	}
+}
